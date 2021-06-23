2e5adec09d022b05f8bea0cb7d2c8b72aae4d4be
PelleLindroth <pellelindroth@gmail.com>
2021-06-23 15:54:31 +0200

    Install and implement expo google fonts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet, View, StatusBar } from 'react-native'
+import { StyleSheet, StatusBar } from 'react-native'
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Righteous_400Regular } from '@expo-google-fonts/righteous'
 import HomeTabs from './navigators/HomeTabs'
+import AppLoading from 'expo-app-loading'
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" />
-      <HomeTabs />
-    </NavigationContainer>
-  )
+  let [fontsLoaded] = useFonts({
+    Righteous_400Regular,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" />
+        <HomeTabs />
+      </NavigationContainer>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
