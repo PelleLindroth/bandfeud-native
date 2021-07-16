a4f3072fb387c1ae84b6dcaf0e5cea0209d1320b
PelleLindroth <pellelindroth@gmail.com>
2021-07-16 10:30:17 +0200

    Add Measure constants

---
+++ src/constants/Measure.ts
@@ -0,0 +1,5 @@
+import { Dimensions } from 'react-native'
+
+export const windowWidth = Dimensions.get("window").width
+export const imageSide = windowWidth * 0.7
+export const listPadding = windowWidth / 2 - imageSide / 2 - 20
