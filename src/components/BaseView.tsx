6df8bfc4b606ca013db49daa0cf1e2380abdd88a
PelleLindroth <pellelindroth@gmail.com>
2021-07-16 10:30:44 +0200

    Create BaseView component

---
+++ src/components/BaseView.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet } from 'react-native'
+import Color from '../constants/Color'
+
+type BaseProps = {
+  children: React.ReactNode,
+  style?: object
+}
+
+const BaseView = (props: BaseProps) => {
+  return (
+    <SafeAreaView style={[styles.wrapper, props.style && props.style]}>
+      {props.children}
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    alignItems: 'center',
+    backgroundColor: Color.offBlack,
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
+
+export default BaseView
