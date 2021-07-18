92edb7862ff42696415eeaf8123dd60edd91db66
PelleLindroth <pellelindroth@gmail.com>
2021-07-18 09:10:00 +0200

    Add constant iconSide

--- src/constants/Measure.ts
+++ src/constants/Measure.ts
@@ -3,3 +3,4 @@ import { Dimensions } from 'react-native'
 export const windowWidth = Dimensions.get("window").width
 export const imageSide = windowWidth * 0.7
 export const listPadding = windowWidth / 2 - imageSide / 2 - 20
+export const iconSide = 40
